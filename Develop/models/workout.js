37a104391c3580dee97e8622962be95b5519873c
Smeza314 <sebastianmeza314@gmail.com>
2021-04-12 16:49:14 -0700

    finished models

---
+++ Develop/models/workout.js
@@ -0,0 +1,45 @@
+const { model, schema, Schema } = require('mongoose')
+
+const workoutSchema = new Schema({
+  day: Date,
+  excercises: [{
+    type: String,
+    name: String,
+    duration: Number,
+    weight: Number,
+    reps: Number,
+    sets: Number,
+    distance: Number
+  }],
+},
+  {typeKey: '$type', toJSON: {virtuals: true}, toObject: {virtuals: true} }
+)
+
+workoutSchema.virtual('totalWeight').get(function () {
+  let total = 0
+
+  this.excercises.forEach(exercise => {
+    total += exercise.weight
+  })
+  return total
+})
+
+workoutSchema.virtual('totalDuration').get(function () {
+  let total = 0
+
+  this.excercises.forEach(exercise => {
+    total += exercise.duration
+  })
+  return total
+})
+
+workoutSchema.virtual('totalWorkouts').get(function () {
+  let total = 0
+
+  this.excercises.forEach(exercise => {
+    total += exercise.name
+  })
+  return total
+})
+
+module.exports = model('workout', workoutSchema)
